5f1139f3a3759d94b8ad6a90e62487dbc556fcd6
Nick Turman-Bryant <nicktb@pdx.edu>
2019-03-18 14:24:40 -0700

    Delete config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-nrel_api_key = 'z4e1xwMrfeQznobqKJuwQJZGQ2nRsPF2uDYKaX0W'
-wund_api_key = '768e63d6ba5a832e'
-owm_key = '5356e349e93bec15f2aa52165dd0c2ae'
-ds_key = '4332a0b728e5162c2e430b5f83f403c9'
